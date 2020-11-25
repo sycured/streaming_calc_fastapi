09496c8d5eab15ccb9a7a53f0a4d6b09f906c1fe
sycured <60801403+sycured@users.noreply.github.com>
2020-11-25 12:08:31 -0500

    add new tests

--- tests/test_bwserver.py
+++ tests/test_bwserver.py
@@ -7,12 +7,13 @@ bitrate = 64
 bitrate_float = 64.8
 nblisteners = 250
 
+bws = '/bwserver'
 client = TestClient(app)
 
 
-def test_1():
+def test_post_int():
     """With int."""
-    response = client.post(url='/bwserver',
+    response = client.post(url=bws,
                            json={'nblisteners': nblisteners,
                                  'bitrate': bitrate}
                            )
@@ -20,11 +21,47 @@ def test_1():
     assert response.json() == {'result': 15625}
 
 
-def test_2():
+def test_post_float():
     """With float."""
-    response = client.post(url='/bwserver',
+    response = client.post(url=bws,
                            json={'nblisteners': nblisteners,
                                  'bitrate': bitrate_float}
                            )
     assert response.status_code == 200
     assert response.json() == {'result': 15820.3125}
+
+
+def test_get():
+    """Test GET request."""
+    response = client.get(url=bws)
+    assert response.status_code == 405
+
+
+def test_options():
+    """Test OPTIONS request."""
+    response = client.options(url=bws)
+    assert response.status_code == 405
+
+
+def test_delete():
+    """Test DELETE request."""
+    response = client.delete(url=bws)
+    assert response.status_code == 405
+
+
+def test_head():
+    """Test HEAD request."""
+    response = client.head(url=bws)
+    assert response.status_code == 405
+
+
+def test_put():
+    """Test PUT request."""
+    response = client.put(url=bws)
+    assert response.status_code == 405
+
+
+def test_patch():
+    """Test PATCH request."""
+    response = client.patch(url=bws)
+    assert response.status_code == 405
--- tests/test_serverusagebw.py
+++ tests/test_serverusagebw.py
@@ -10,11 +10,12 @@ nbhours = 24
 nblisteners = 250
 
 client = TestClient(app)
+svusbw = '/serverusagebw'
 
 
-def test_1():
+def test_post_int():
     """With int."""
-    response = client.post(url='/serverusagebw',
+    response = client.post(url=svusbw,
                            json={'bitrate': bitrate,
                                  'nbdays': nbdays,
                                  'nbhours': nbhours,
@@ -25,9 +26,9 @@ def test_1():
     assert response.json() == {'result': 164794.921875}
 
 
-def test_2():
+def test_post_float():
     """With float."""
-    response = client.post(url='/serverusagebw',
+    response = client.post(url=svusbw,
                            json={'bitrate': bitrate_float,
                                  'nbdays': nbdays,
                                  'nbhours': nbhours,
@@ -36,3 +37,39 @@ def test_2():
                            )
     assert response.status_code == 200
     assert response.json() == {'result': 166854.8583984375}
+
+
+def test_get():
+    """Test GET request."""
+    response = client.get(url=svusbw)
+    assert response.status_code == 405
+
+
+def test_options():
+    """Test OPTIONS request."""
+    response = client.options(url=svusbw)
+    assert response.status_code == 405
+
+
+def test_delete():
+    """Test DELETE request."""
+    response = client.delete(url=svusbw)
+    assert response.status_code == 405
+
+
+def test_head():
+    """Test HEAD request."""
+    response = client.head(url=svusbw)
+    assert response.status_code == 405
+
+
+def test_put():
+    """Test PUT request."""
+    response = client.put(url=svusbw)
+    assert response.status_code == 405
+
+
+def test_patch():
+    """Test PATCH request."""
+    response = client.patch(url=svusbw)
+    assert response.status_code == 405

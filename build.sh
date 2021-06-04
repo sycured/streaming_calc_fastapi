3170f9915031e1a3be8118f1c254ee899c5ef54f
sycured <60801403+sycured@users.noreply.github.com>
2021-06-04 14:49:57 -0500

    move to nginx unit

--- build.sh
+++ build.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
 set -xe
 
-mkbuild=$(buildah from docker.io/centos:latest)
-buildah run "$mkbuild" -- dnf upgrade -y
-buildah run "$mkbuild" -- dnf install curl gcc make python3 python3-pip python3-devel -y
-mntbuild=$(buildah mount "$mkbuild")
-mkdir "$mntbuild"/dependencies
-cp requirements.txt "$mntbuild"/opt
-buildah run "$mkbuild" -- bash -c "curl --proto '=https' --tlsv1.2 -sSf -o /tmp/rustup.sh https://sh.rustup.rs && chmod +x /tmp/rustup.sh && /tmp/rustup.sh -y && echo 'PATH=$HOME/.cargo/env:$PATH' >> ~/.bashrc"
-buildah run "$mkbuild" -- bash -c "source $HOME/.cargo/env && rustup default nightly"
-buildah run "$mkbuild" -- pip3 install --no-cache-dir --install-option="--prefix=/dependencies" maturin
-buildah run "$mkbuild" -- pip3 install --no-cache-dir --install-option="--prefix=/dependencies" -r /opt/requirements.txt
-
-mkfinal=$(buildah from scratch)
-buildah config --author='sycured' "$mkfinal"
-buildah config --label Name='streaming-calc-fastapi' "$mkfinal"
-buildah config --workingdir='/opt' "$mkfinal"
-buildah config --cmd 'uvicorn --host 0.0.0.0 --port 8000 main:app' "$mkfinal"
-mntfinal=$(buildah mount "$mkfinal")
-mkdir -p "$mntfinal"/opt
-yum --nogpgcheck --installroot="$mntfinal" --repofrompath centos8,http://mirror.centos.org/centos/8/BaseOS/x86_64/os/ --repofrompath appstream,http://mirror.centos.org/centos/8/AppStream/x86_64/os/ --repofrompath  extras,http://mirror.centos.org/centos/8/extras/x86_64/os/ --repo=centos8,appstream, install glibc-langpack-en python3 -y
-cp -r "$mntbuild"/dependencies "$mntfinal"/usr/local
-cp main.py "$mntfinal"/opt
-
-buildah unmount "$mkbuild"
-buildah unmount "$mkfinal"
-buildah rm "$mkbuild"
-buildah commit --squash "$mkfinal" "scfa"
-buildah rm "$mkfinal"
+mkimg=$(buildah from docker.io/nginx/unit:1.24.0-python3.9)
+buildah config --author='sycured' "$mkimg"
+buildah config --label Name='streaming-calc-fastapi' "$mkimg"
+buildah config --port 8000 "$mkimg"
+buildah copy --chown unit:unit "$mkimg" config.json /docker-entrypoint.d/config.json
+buildah copy --chown unit:unit "$mkimg" requirements.txt /opt/requirements.txt
+buildah run "$mkimg" -- pip install --no-cache-dir -r /opt/requirements.txt
+buildah copy --chown unit:unit "$mkimg" main.py /opt
+buildah commit --squash "$mkimg" "scfa"
+buildah rm "$mkimg"

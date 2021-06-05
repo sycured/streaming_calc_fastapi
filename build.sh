9e2c523f7687fa0ab0c8e4a410d688c2d13474ec
sycured <60801403+sycured@users.noreply.github.com>
2021-06-04 20:08:40 -0500

    add label org.opencontainers.image.source

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -xe
 
 mkimg=$(buildah from docker.io/nginx/unit:1.24.0-python3.9)
 buildah config --author='sycured' "$mkimg"
-buildah config --label Name='streaming-calc-fastapi' "$mkimg"
+buildah config --label Name='streaming-calc-fastapi' --label org.opencontainers.image.source="https://github.com/sycured/streaming_calc_fastapi" "$mkimg"
 buildah config --port 8000 "$mkimg"
 buildah copy --chown unit:unit "$mkimg" config.json /docker-entrypoint.d/config.json
 buildah copy --chown unit:unit "$mkimg" requirements.txt /opt/requirements.txt
